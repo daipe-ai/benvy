3466591468d2f838fcd64a73aaab43fb2fa0f071
Jiri Koutny <jirkakoutny@gmail.com>
2020-12-12 06:15:37 +0100

    DatabricksConnectDetector uses file_contains_string()

--- src/benvy/databricks/DatabricksConnectDetector.py
+++ src/benvy/databricks/DatabricksConnectDetector.py
@@ -1,11 +1,9 @@
 import os
+from penvy.string.string_in_file import file_contains_string
 
 
 class DatabricksConnectDetector:
     def detect(self):
         poetry_lock_path = os.getcwd() + os.sep + "poetry.lock"
 
-        with open(poetry_lock_path, "r", encoding="utf-8") as f:
-            content = f.read()
-
-        return 'name = "databricks-connect"' in content
+        return file_contains_string('name = "databricks-connect"', poetry_lock_path)
