fa548978d3ace0adf7f0775c198b0d6afb252852
Karel Gudera <karel.gudera@datasentics.com>
2022-02-19 11:24:22 +0100

    Added locking for case of concurrent notebooks runs on same cluster

--- src/benvy/databricks/repos/install/PoetryDownloader.py
+++ src/benvy/databricks/repos/install/PoetryDownloader.py
@@ -2,6 +2,7 @@ import urllib
 from pathlib import Path
 from logging import Logger
 from penvy.setup.SetupStepInterface import SetupStepInterface
+from benvy.mutex.Mutex import Mutex
 
 
 class PoetryDownloader(SetupStepInterface):
@@ -18,9 +19,13 @@ class PoetryDownloader(SetupStepInterface):
         self._logger = logger
 
     def run(self):
-        self._logger.info(f"Downloading poetry {self._poetry_version}")
-        Path(self._download_path).parent.mkdir(parents=True, exist_ok=True)
-        urllib.request.urlretrieve(self._download_url, self._download_path)
+        with Mutex("benvy_poetry_downloader_mutex", timeout=180):
+            if not self.should_be_run():
+                return
+
+            self._logger.info(f"Downloading poetry {self._poetry_version}")
+            Path(self._download_path).parent.mkdir(parents=True, exist_ok=True)
+            urllib.request.urlretrieve(self._download_url, self._download_path)
 
     def get_description(self):
         return f"Download poetry {self._poetry_version}"
--- src/benvy/databricks/repos/install/PoetryInstallScriptDownloader.py
+++ src/benvy/databricks/repos/install/PoetryInstallScriptDownloader.py
@@ -2,6 +2,7 @@ import urllib
 from pathlib import Path
 from logging import Logger
 from penvy.setup.SetupStepInterface import SetupStepInterface
+from benvy.mutex.Mutex import Mutex
 
 
 class PoetryInstallScriptDownloader(SetupStepInterface):
@@ -16,9 +17,13 @@ class PoetryInstallScriptDownloader(SetupStepInterface):
         self._logger = logger
 
     def run(self):
-        self._logger.info("Downloading poetry install script")
-        Path(self._download_path).parent.mkdir(parents=True, exist_ok=True)
-        urllib.request.urlretrieve(self._download_url, self._download_path)
+        with Mutex("benvy_poetry_install_script_downloader_mutex", timeout=180):
+            if not self.should_be_run():
+                return
+
+            self._logger.info("Downloading poetry install script")
+            Path(self._download_path).parent.mkdir(parents=True, exist_ok=True)
+            urllib.request.urlretrieve(self._download_url, self._download_path)
 
     def get_description(self):
         return "Download poetry installation script"
